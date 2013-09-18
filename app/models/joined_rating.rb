a6ac9363c34d79b95cc416810372682aca87a8ed
Christian Oschwald <christian.oschwald@txtr.com>
2013-09-18 15:04:21 +0200

    nicer graph with default options

--- app/models/joined_rating.rb
+++ app/models/joined_rating.rb
@@ -9,5 +9,13 @@ class JoinedRating < ActiveRecord::Base
   	android_rating + itunes_rating
   end
 
+  def android_rating_nice
+  	(sprintf "%.2f", android_rating).to_f
+  end
+
+  def itunes_rating_nice
+  	(sprintf "%.2f", itunes_rating).to_f
+  end
+
   include Rating
 end
