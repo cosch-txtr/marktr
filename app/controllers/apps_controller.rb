7b375f5774f197ea6ccd1c38338f687a1d721e79
Christian Oschwald <christian.oschwald@txtr.com>
2013-09-18 12:44:17 +0200

    start on all

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,5 +1,13 @@
 class AppsController < ApplicationController
 
+	def all
+		@ratings = []
+		App.android_itunes.each do |app|
+			@ratings += app.android_ratings
+			@ratings += app.itunes_ratings
+		end
+	end
+
 	def android
 		@ratings = []
 		App.android.each do |app|
--- app/models/app.rb
+++ app/models/app.rb
@@ -3,12 +3,16 @@ class App < ActiveRecord::Base
 	has_many :android_ratings
 	has_many :itunes_ratings
 
+	def self.android_itunes
+		where("android_id not NULL AND ios_id not NULL").load
+	end
+
 	def self.android
-		where("android_id not NULL").all
+		where("android_id not NULL").load
 	end
 
 	def self.itunes
-		where("ios_id not NULL").all
+		where("ios_id not NULL").load
 	end
 
 end
