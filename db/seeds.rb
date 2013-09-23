3acbb0e58c13d5633c872f86b13cc8d855ebb1e7
Christian Oschwald <christian.oschwald@txtr.com>
2013-09-23 13:10:57 +0200

    seed can now be runned multiple times -> avoid duplicates

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,39 +8,39 @@
 
 App.create( { :name=>"txtr", :android_id=>"com.txtr.android", 
 			  :itunes_id=>"564715781", :itunes_country=>"de", 
-			  :win8_id=>"txtr-reader/1e3b5a45-302f-4e99-a4d3-9a2371c2a395", :win8_country=>"de-de" })
+			  :win8_id=>"txtr-reader/1e3b5a45-302f-4e99-a4d3-9a2371c2a395", :win8_country=>"de-de" }) if !App.find_by_name "txtr"
 
 App.create( { :name=>"kobo", :android_id=>"com.kobobooks.android", 
 			  :itunes_id=>"301259483", :itunes_country=>"us",
-			  :win8_id=>"kobo/05e8583d-b242-43be-8dcf-c8a9c49d8ee3", :win8_country=>"en-us" } )
+			  :win8_id=>"kobo/05e8583d-b242-43be-8dcf-c8a9c49d8ee3", :win8_country=>"en-us" } ) if !App.find_by_name "kobo"
 
 App.create( { :name=>"nook", :android_id=>"bn.ereader", 
 			  :itunes_id=>"373582546", :itunes_country=>"us" ,
-			  :win8_id=>"nook-books-magazines-newspapers/05dbbb07-cd42-4a5f-9cd3-a329d52bd372", :win8_country=>"en-us" } )                                                                               
+			  :win8_id=>"nook-books-magazines-newspapers/05dbbb07-cd42-4a5f-9cd3-a329d52bd372", :win8_country=>"en-us" } ) if !App.find_by_name "nook"
 
 App.create( { :name=>"kindle", :android_id=>"com.amazon.kindle", 
 			  :itunes_id=>"302584613", :itunes_country=>"us",    
-			  :win8_id=>"kindle/1d7e4396-0143-4aed-8892-84eb75e799f3", :win8_country=>"en-us" } )                                                                              
+			  :win8_id=>"kindle/1d7e4396-0143-4aed-8892-84eb75e799f3", :win8_country=>"en-us" } ) if !App.find_by_name "kindle"
 
 App.create( { :name=>"playbooks", :android_id=>"com.google.android.apps.books", 
-			  :itunes_id=>"400989007", :itunes_country=>"us" } )                                                                  
+			  :itunes_id=>"400989007", :itunes_country=>"us" } ) if !App.find_by_name "playbooks"
 
 App.create( { :name=>"libri", :android_id=>"de.ebook.reader", 
-			  :itunes_id=>"557173140", :itunes_country=>"de" } )
+			  :itunes_id=>"557173140", :itunes_country=>"de" } ) if !App.find_by_name "libri"
 
 App.create( { :name=>"pageplace", :android_id=>"de.telekom.epub", 
-	  		  :itunes_id=>"415877578", :itunes_country=>"de" } )
+	  		  :itunes_id=>"415877578", :itunes_country=>"de" } ) if !App.find_by_name "pageplace"
 
 App.create( { :name=>"thalia", :android_id=>"com.freiheit.thalia.mobile", 
-			  :itunes_id=>"350742971", :itunes_country=>"de" } )
+			  :itunes_id=>"350742971", :itunes_country=>"de" } ) if !App.find_by_name "thalia"
 
 App.create( { :name=>"weltbild", :android_id=>"de.weltbild.android.ebookreader", 
-			  :itunes_id=>"435921458", :itunes_country=>"de" } )
+			  :itunes_id=>"435921458", :itunes_country=>"de" } ) if !App.find_by_name "weltbild"
 
 App.create( { :name=>"ciando", :android_id=>"com.ciando.ciandoreader", 
-	 		  :itunes_id=>"518274502", :itunes_country=>"de" })
+	 		  :itunes_id=>"518274502", :itunes_country=>"de" }) if !App.find_by_name "ciando"
 
-App.create( { :name=>"iBooks", :itunes_id=>"364709193", :itunes_country=>"us" } )
+App.create( { :name=>"iBooks", :itunes_id=>"364709193", :itunes_country=>"us" } ) if !App.find_by_name "iBooks"
 
 
 a=App.find_by_name "txtr"
