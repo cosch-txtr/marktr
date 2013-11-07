5b8dee70655ec9be09452ef9ae4bfb7bc2f82266
Christian Oschwald <christian.oschwald@txtr.com>
2013-11-07 14:18:18 +0100

    added downloads, total_downloads fetcher

--- lib/appfigures_loader.rb
+++ lib/appfigures_loader.rb
@@ -7,27 +7,16 @@ require "uri"
 class AppfiguresLoader
 
 	def self.rank_now(app, c)
-		id = app.itunes_appfig_id
-		country = c.itunes_country
-		date = DateTime.now.strftime("%Y-%m-%d")
-		uri = URI.parse("https://api.appfigures.com/v2/ranks/#{id}/daily/#{date}/#{date}?countries=#{country}&tz=utc")
- 	
-		http = Net::HTTP.new(uri.host, uri.port)
-		http.use_ssl = true
-		http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-
-		request = Net::HTTP::Get.new(uri.request_uri)
-		request["X-Client-Key"] = ENV["APPFIG_APP_KEY"]
-		request["Authorization"] = ENV["APPFIG_AUTH"]
-
-		response = http.request(request)
+		app_country(app,c)
+		uri = URI.parse("https://api.appfigures.com/v2/ranks/#{@id}/daily/#{@date}/#{@date}?countries=#{@country}&tz=utc")
+		
+		response = do_request(uri)
 
 		result = JSON.parse(response.body) if response.code == "200"
 
 		if result && result["data"] && result["data"].size!=0		  
 		  #puts uri
 		  #pp result
-		  #puts "#{result["data"][1]["category"]["device"]}"
 
 		  { 
 		  	:"#{result["data"][0]["category"]["device"]}" => result["data"][0]["positions"][0],
@@ -37,5 +26,66 @@ class AppfiguresLoader
 		  puts response.inspect
 		end
 	end
+
+	def self.downloads(app, c)
+		app_country(app,c)
+		uri=URI.parse("https://api.appfigures.com/v2/sales?start_date=#{@date}&end_date=#{@date}&granularity=daily&products=#{@id}");
+		response = do_request(uri)
+
+		result = JSON.parse(response.body) if response.code == "200"
+
+		if result && result["downloads"] && result["updates"]		  
+		  #puts uri
+		  #pp result
+
+		  { 
+		  	:downloads => result["downloads"],
+		  	:updates => result["updates"]
+		  }
+		else
+		  puts response.inspect
+		end
+	end
+
+
+	def self.total_downloads(app, c)
+		app_country(app,c)
+		uri=URI.parse("https://api.appfigures.com/v2/sales?products=#{@id}");
+		response = do_request(uri)
+
+		result = JSON.parse(response.body) if response.code == "200"
+
+		if result && result["downloads"] && result["updates"]		  
+		  #puts uri
+		  #pp result
+
+		  { 
+		  	:total_downloads => result["downloads"],
+		  	:total_updates => result["updates"]
+		  }
+		else
+		  puts response.inspect
+		end
+	end
+
+
+private
 	
+	def self.app_country( app, c )
+		@id = app.itunes_appfig_id
+		@country = c.itunes_country
+		@date = DateTime.now.yesterday.strftime("%Y-%m-%d")
+	end
+
+	def self.do_request(uri)
+		http = Net::HTTP.new(uri.host, uri.port)
+		http.use_ssl = true
+		http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+		request = Net::HTTP::Get.new(uri.request_uri)
+		request["X-Client-Key"] = ENV["APPFIG_APP_KEY"]
+		request["Authorization"] = ENV["APPFIG_AUTH"]
+
+		http.request(request)
+	end
 end
